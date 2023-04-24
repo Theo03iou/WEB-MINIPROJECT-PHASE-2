5449631f48fcb196b687ab269d0e056668a2e662
Theo Georgiou <75097082+Theo03iou@users.noreply.github.com>
2023-04-24 17:59:29 +0100

    poop

---
+++ WEB-MINIPROJECT-main/PHP/addPost.php
@@ -0,0 +1,6 @@
+The input data will then need to be processed
+by a PHP script named addPost.php.
+• This script should save the post into a table
+within a database (which you created in topic 9)
+and then redirect the page to another PHP
+document named viewBlog.php
